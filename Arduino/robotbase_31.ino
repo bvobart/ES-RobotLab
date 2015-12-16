c95e787f49ed5a430623751557ab2d02ac5ead8e
Steven Meijer <stevenmeijer@outlook.com>
2015-12-16 12:28:17 +0100

    Added initial Arduino file

---
+++ Arduino/robotbase_31.ino
@@ -0,0 +1,19 @@
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+int LEFT_FWD = 6;
+int LEFT_BCK = 7;
+int LEFT_ENABLE = 24;
+int RIGHT_FWD = 2;
+int RIGHT_BCK = 3;
+int RIGHT_ENABLE = 25;
+int BLUE_TX = 18;
+int BLUE_RX = 19;
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
