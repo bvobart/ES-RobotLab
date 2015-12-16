fcfb898fc7c961cc8efcd2d5558324b7e2e120ed
Steven Meijer <stevenmeijer@outlook.com>
2015-12-16 13:58:40 +0100

    Added all arduino ports

---
+++ Arduino/robotbase_31/robotbase_31.ino
@@ -0,0 +1,22 @@
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+int LEFT_FWD = 6;        // LCHB-100 H Bridge 1FWD
+int LEFT_BCK = 7;        // LCHB-100 H Bridge 1REV
+int LEFT_ENABLE = 24;    // LCHB-100 H Bridge 1EN
+int RIGHT_FWD = 2;       // LCHB-100 H Bridge 2FWD
+int RIGHT_BCK = 3;       // LCHB-100 H Bridge 2REV
+int RIGHT_ENABLE = 25;   // LCHB-100 H Bridge 2EN
+int BLUETOOTH_TX = 18;   // HC-05 Bluetooth dongle TX
+int BLUETOOTH_RX = 19;   // HC-05 Bluetooth dongle RX
+int US_ECHO = 22;        // HC-SR04 ultrasonic sensor ECHO
+int US_TRIGGER = 23;     // HC-SR04 ultrasonic sensor TRIGGER
+int LED = 13;            // Yellow LED light
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
