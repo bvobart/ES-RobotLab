fedba52020487e017bc4d340569944720d60a653
Bart van Oort <bvanoort@chello.nl>
2016-01-04 20:10:20 +0100

    Now sends the correct Twist messages.

--- msgServer/src/server.cpp
+++ msgServer/src/server.cpp
@@ -4,17 +4,56 @@
 #include <cstdio>
 #include <sstream>
 
+double speed = 5.0;
+
 /**
- * Returns a Twist message.
+ * Returns a Twist message according to the character pressed.
+ *
+ * The X in the linear component is positive for forwards,
+ * and negative for backwards.
+ *
+ * The X in the angular component is positive for turning left,
+ * and negative for turning right.
+ *
+ * Both are scaled according to the speed, which initializes to 5
+ * and can be incremented or decremented using the x and z keys
+ * respectively. r resets the speed.
  */
 geometry_msgs::Twist getTwist(char c) {
 	geometry_msgs::Twist msg;
-	msg.linear.x = 1.0;
+	switch (tolower(c)) {
+		case 'w': //forward
+			msg.linear.x = speed;
+			break;
+		case 's': //backward
+			msg.linear.x = -speed;
+			break;
+		case 'a': //left
+			msg.angular.x = speed;
+			break;
+		case 'd': //right
+			msg.angular.x = -speed;
+			break;
+		case 'z': //slow down
+			if (speed > 0)
+				speed -= 0.5;
+			break;
+		case 'x': //speed up
+			if (speed < 10)
+				speed += 0.5;
+			break;
+		case 'r': //reset speed
+			speed = 5.0;
+			break;
+		default:
+			ROS_INFO("Unknown key. Speed is %f", speed);
+			break;
+	}
 	return msg;
 }
 
 /**
- * This should send Twist messages according to what key you press.
+ * This sends Twist messages according to what key you press.
  */
 int main(int argc, char **argv) {
   ros::init(argc, argv, "server");
@@ -29,9 +68,6 @@ int main(int argc, char **argv) {
     c = getchar();
     
     geometry_msgs::Twist msg = getTwist(c);
-
-    ROS_INFO("Linear: %f %f %f", msg.linear.x, msg.linear.y, msg.linear.z);
-    ROS_INFO("Angular: %f %f %f", msg.angular.x, msg.angular.y, msg.angular.z);
 
     chatter_pub.publish(msg);
     ros::spinOnce();
